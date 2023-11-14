24eea638be0a2637c60c113513ae420cc7c4bb30
Nnamdi Michael Okpala <okpalan86@gmail.com>
2023-11-14 21:02:06 +0000

    a

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,9 @@
 // cdnjs
 import "https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js";
-import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
-
-
 import "./scss/styles.scss";
 
 // components
 import "./components/chart/chart.js";
 import "./components/eye/eye.js";
-import "./components/voronoi/voronoi.js";
 
 import "./scripts/index.js";
