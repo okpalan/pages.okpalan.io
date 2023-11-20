b8d7f6e878c11f998976d07ac814f8cc68baa381
Nnamdi Michael Okpala <okpalan86@gmail.com>
2023-11-20 20:20:00 +0000

    a

--- client/src/scripts/ui/preload.js
+++ client/src/scripts/ui/preload.js
@@ -1,6 +1,5 @@
-import yepnope from "yepnope";
-const testJS = () => (typeof navigator !== 'undefined' &&
-    navigator.userAgent &&
-    navigator.userAgent.indexOf('Mozilla/5.0') >= 0 &&
-    navigator.userAgent.indexOf('Trident/') < 0 &&
-    navigator.userAgent.indexOf('Edge/') < 0);
+// const testJS = () => (typeof navigator !== 'undefined' &&
+//     navigator.userAgent &&
+//     navigator.userAgent.indexOf('Mozilla/5.0') >= 0 &&
+//     navigator.userAgent.indexOf('Trident/') < 0 &&
+//     navigator.userAgent.indexOf('Edge/') < 0);
